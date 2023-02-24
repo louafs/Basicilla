ea585df6b48698f10acf11e35a269a23b749063f
louafs <lafshriver@gmail.com>
2023-02-23 20:16:00 -0800

    final

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,8 +1,34 @@
-var modal = document.querySelector(".modal");
+// var modal = document.querySelector(".modal");
+// var modalOuter = document.querySelector(".modal-outer");
+
+// export function showPopover(event) {
+//   if (!event.target.matches(".beta, .closer")) return;
+//   modalOuter.classList.toggle("open");
+//   event.preventDefault();
+// }
+
 var modalOuter = document.querySelector(".modal-outer");
+var modalInner = document.querySelector(".modal");
+
+var betaContent = `
+<h3>Oooops!</h3>
+<p>Wow! Nothing works!<p>
+`;
+var buttonContent = `
+<h2>Coming Soon</h2>
+<p>This feature coming soon.<p>
+<a class="closer" href="#0">✖︎</a>
+`;
 
 export function showPopover(event) {
-  if (!event.target.matches(".beta, .closer")) return;
-  modalOuter.classList.toggle("open");
+  if (event.target.matches(".beta")) {
+    modalInner.innerHTML = betaContent;
+    modalOuter.classList.add("open");
+  } else if (event.target.closest("nav ul")) {
+    modalInner.innerHTML = buttonContent;
+    modalOuter.classList.add("open");
+  } else if (event.target.matches(".closer, .modal-outer")) {
+    modalOuter.classList.remove("open");
+  } else return;
   event.preventDefault();
 }
